9447773d53eab8b4a6f138466df58aa41174b333
andresjaramillo7 <a01029079@tec.mx>
2025-12-03 21:09:11 -0600

    fastapi added robocap function to connect the model to get a caption and return it

--- api/main.py
+++ api/main.py
@@ -1,7 +1,6 @@
 from fastapi import FastAPI, UploadFile, File
 from pathlib import Path
-import time
-
+from src.data.load_data import Robocap
 app = FastAPI()
 
 UPLOAD_DIR = Path("uploads")
@@ -10,11 +9,6 @@ UPLOAD_DIR.mkdir(parents=True, exist_ok=True)
 @app.post("/caption")
 async def caption(image: UploadFile = File(...)):
     contents = await image.read()
-    print("[SERVER] got file:", image.filename, "bytes:", len(contents))
-
-    filename = f"{int(time.time()*1000)}_{image.filename or 'photo.jpg'}"
-    out_path = UPLOAD_DIR / filename
-    out_path.write_bytes(contents)
-    print("[SERVER] saved to:", out_path.resolve())
-
-    return {"caption": "upload works (dummy caption)"}
+    cap = Robocap(contents)
+    
+    return {"caption": cap}
